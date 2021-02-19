e0ea0792073f9123c97cc0337121e27bcf9433bf
Rafal Gumienny <guma44@gmail.com>
2021-02-19 21:39:11 +0100

    Removed social accounts

--- brivo/users/adapters.py
+++ brivo/users/adapters.py
@@ -1,7 +1,6 @@
 from typing import Any
 
 from allauth.account.adapter import DefaultAccountAdapter
-from allauth.socialaccount.adapter import DefaultSocialAccountAdapter
 from django.conf import settings
 from django.http import HttpRequest
 
@@ -10,7 +9,3 @@ class AccountAdapter(DefaultAccountAdapter):
     def is_open_for_signup(self, request: HttpRequest):
         return getattr(settings, "ACCOUNT_ALLOW_REGISTRATION", True)
 
-
-class SocialAccountAdapter(DefaultSocialAccountAdapter):
-    def is_open_for_signup(self, request: HttpRequest, sociallogin: Any):
-        return getattr(settings, "ACCOUNT_ALLOW_REGISTRATION", True)
