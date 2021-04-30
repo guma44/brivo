9d1c9c0e6fcaf203a4a44c264f8d8ae69fe70746
Rafal Gumienny <guma44@gmail.com>
2021-04-30 21:58:30 +0200

    Add pagination to API

--- config/settings/base.py
+++ config/settings/base.py
@@ -310,6 +310,8 @@ REST_FRAMEWORK = {
         "rest_framework.authentication.SessionAuthentication",
         "rest_framework.authentication.TokenAuthentication",
     ),
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
+    'PAGE_SIZE': 20,
     "DEFAULT_PERMISSION_CLASSES": ("rest_framework.permissions.IsAuthenticated",),
 }
 
